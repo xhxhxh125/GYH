a3f76c0f43e41d85bf4802a9d4656619e952d9dc
zhhlxh <13675132313@163.com>
2019-03-25 16:10:50 +0800

    first commit

---
+++ Unify261/html/assets/js/custom.js
@@ -0,0 +1,55 @@
+ $(document).ready(function() {
+/**
+ * 我的收藏页面
+ * 取消收藏
+ *  */
+//     $(".delect-collect").on('click',function(e){
+//         $(this).parent('td').parent('tr').remove()
+//    });
+   /**
+    * 公司新闻-编辑新闻页面
+    * 删除上传的文件
+    *  */
+//    $('.delect-icon').on('click',function (e) { 
+//        $(this).parent('.upload-files').remove()
+//     })
+    // function editData(obj){
+    //     console.log(1111111);
+        
+    //     alert(obj.id);
+    // }
+
+    // delDialog =  document.querySelector('#del-dialog')
+    // editDialog =  document.querySelector('#edit-dialog')
+      
+    
+    // document.querySelector("#close").onclick = function(){
+    //     // var val = document.querySelector("#return_value").value;
+    //     delDialog.close();
+    // };
+    // document.querySelector("#del-close").onclick = function(){
+    //     // var val = document.querySelector("#return_value").value;
+    //     delDialog.close();
+    // };
+    // document.querySelector("#edit-close").onclick = function(){
+    //     // var val = document.querySelector("#return_value").value;
+    //     editDialog.close();
+    // };
+    
+    // editDialog.addEventListener("close", function(e){
+    //     if(this.returnValue === "是"){
+    //         alert(this.returnValue)
+    //         //dosomething...
+    //     }else{
+    //         alert(this.returnValue)
+    //         //dosomething...
+    //     };
+    // });
+});
+// var delDialog,editDialog
+// function showDelDialog(obj){
+//     delDialog.showModal()
+// }
+// function showEditDialog(obj){
+//     editDialog.showModal()
+// }
